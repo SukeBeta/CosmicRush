5bdb6358b00155861bf718e6f8027760cfce8a0a
Geyang <babxyq@gmail.com>
2015-09-21 21:33:24 +1000

    Add progress bar

--- client/game/states/Preloader.js
+++ client/game/states/Preloader.js
@@ -5,46 +5,37 @@ BasicGame.Preloader = function (game) {
 
 };
 
+// highscore
+var highscore = 0;
+
+// gameStart indicator
+var gameStart = false;
+
+// game state indicator
+var gameState = false;
+
+
 BasicGame.Preloader.prototype = {
 
     preload: function () {
-        this.game.load.image('p01', 'game/assets/p01.png');
-        this.game.load.image('p02', 'game/assets/p02.png');
-        this.game.load.image('p03', 'game/assets/p03.png');
+        this.game.stage.backgroundColor = '#100826';
+
+        var loadingLabel = this.game.add.text(this.game.world.centerX, this.game.world.centerY - 100, 'loading...', {font: '30px Arial', fill: '#ffffff'});
+        loadingLabel.anchor.setTo(0.5, 0.5);
+
+        var progressBar = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'progressBar');
+        progressBar.anchor.setTo(0.5, 0.5);
+        this.game.load.setPreloadSprite(progressBar);
     },
 
     create: function () {
     },
 
     update: function () {
-        this.state.start('Game');
+        this.start();
+    },
+
+    start: function() {
+        this.game.state.start('Menu');
     }
 };
-
-/**
- * Old preloader
- */
-//BasicGame.Preloader.prototype = {
-//
-//    preload: function () {
-//        this.game.load.image('background','game/assets/debug-grid-1920x1920.png');
-//
-//        this.game.load.image('p01', 'game/assets/p01.png');
-//        this.game.load.image('p02', 'game/assets/p02.png');
-//        this.game.load.image('p03', 'game/assets/p03.png');
-//        this.game.load.image('p04', 'game/assets/p04.png');
-//        this.game.load.image('p05', 'game/assets/p05.png');
-//        this.game.load.image('p06', 'game/assets/p06.png');
-//        this.game.load.image('p07', 'game/assets/p07.png');
-//        this.game.load.image('p08', 'game/assets/p08.png');
-//        this.game.load.image('p09', 'game/assets/p09.png');
-//    },
-//
-//    create: function () {
-//
-//    },
-//
-//    update: function () {
-//        this.state.start('Game');
-//    }
-//};
